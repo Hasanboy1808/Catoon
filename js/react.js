d4bd082726c47012839d51999e769717b0f5c237
Hasanboy1808 <98078640+Hasanboy1808@users.noreply.github.com>
2023-01-20 18:48:39 +0500

    edited

---
+++ js/react.js
@@ -0,0 +1,7 @@
+let tn = document.getElementsByClassName('btn10')[0],
+    modal = document.getElementsByClassName('contact')[0];
+
+
+tn.addEventListener('click',function () {
+    modal.style.display = 'block'
+})
