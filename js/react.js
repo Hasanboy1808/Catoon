4a3329f7aea714b6645a220a0f7af0e578df971d
Hasanboy1808 <98078640+Hasanboy1808@users.noreply.github.com>
2023-01-21 13:39:02 +0500

    edited

--- js/react.js
+++ js/react.js
@@ -2,7 +2,7 @@ let tn = document.getElementsByClassName('btn10')[0],
     modal = document.getElementsByClassName('contact')[0],
     x = document.getElementById('x'),
     navburger = document.getElementsByClassName('menu380px')[0];
-    console.log(navburger);
+    menubar = document.getElementsByClassName('navburger')[0];
 
 
 
@@ -17,6 +17,7 @@ x.addEventListener('click', function(){
 
 
 navburger.addEventListener('click',function () {
-    
+    menubar.style.display = 'block'
+
     
 })
