c9c1148dad490403c431cab4657dc593a9f74951
Hasanboy1808 <98078640+Hasanboy1808@users.noreply.github.com>
2023-01-21 13:09:10 +0500

    edited

--- js/react.js
+++ js/react.js
@@ -1,7 +1,22 @@
 let tn = document.getElementsByClassName('btn10')[0],
-    modal = document.getElementsByClassName('contact')[0];
+    modal = document.getElementsByClassName('contact')[0],
+    x = document.getElementById('x'),
+    navburger = document.getElementsByClassName('menu380px')[0];
+    console.log(navburger);
+
 
 
 tn.addEventListener('click',function () {
     modal.style.display = 'block'
+})
+
+x.addEventListener('click', function(){
+    modal.style.display = 'none'
+})
+
+
+
+navburger.addEventListener('click',function () {
+    
+    
 })
